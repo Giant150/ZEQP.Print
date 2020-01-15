a1d4c8f8388002ca3309ac03b5285c9332d3297d
刘巨 <liuju150@outlook.com>
2020-01-15 10:18:35 +0800

    web

---
+++ ZEQP.Print/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZEQP.Print.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
