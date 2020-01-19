e0c7d0a853f021762de2659440dac08dc8b65ad2
刘巨 <liuju150@outlook.com>
2020-01-19 11:00:28 +0800

    field

---
+++ ZEQP.Print/Controllers/FieldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZEQP.Print.Controllers
+{
+    public class FieldController : Controller
+    {
+        public IActionResult Index(int tempId)
+        {
+            ViewBag.TemplateId = tempId;
+            return View();
+        }
+    }
+}
